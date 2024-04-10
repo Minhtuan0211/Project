2d49820709e4b41fe0f36b35b1034e9695040c78
Tuan Nguyen <tuan212104@gmail.com>
2024-04-11 01:22:19 +0700

    My project

---
+++ js/user.js
@@ -0,0 +1,23 @@
+const listUser = [
+  {
+    username: "admin",
+    password: "admin",
+    isAdmin: true,
+  },
+  {
+    username: "lephuc",
+    password: "12345",
+  },
+  {
+    username: "phucquynh",
+    password: "12345",
+  },
+  {
+    username: "tandat",
+    password: "12345",
+  },
+  {
+    username: "quanghuy",
+    password: "12345",
+  },
+];
